38cea634575eb23bb516250b0e1c2a5847111ef8
MOSA-ISSA <124443901+MOSA-ISSA@users.noreply.github.com>
2025-01-01 21:17:32 +0200

    test

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
+
 const mongooseLink = process.env.MI_STORE_MONGOOSE_LINK;
 
 if (!mongooseLink) {
@@ -50,8 +57,7 @@ process.on('SIGINT', async () => {
 });
 
 app.get('/test', async (req, res) => {
-    const result = await sendVerificationEmail('mosasenio@gmail.com', '123456');
-    res.send(result);
+    res.status(200).json({ success: true, message: "test successful" });
 });
 
 // app.use("/", Routes)
