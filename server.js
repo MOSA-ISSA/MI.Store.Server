0d16aa3a2f382f6559a2a9f14a44872a4ace7c15
MOSA-ISSA <124443901+MOSA-ISSA@users.noreply.github.com>
2025-02-09 17:26:29 +0200

    Stopped tracking server.js file

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const app = require("./app");
-const port = process.env.port || 2999;
-const server = http.createServer(app);
-
-app.listen(port);
-
-module.exports = server;
